69fca6a0bb735cfe4e572c76c5910af129fee7b8
Aleh Kasiankou <oleg.kosenkov@innowise-group.com>
2023-03-06 12:22:43 +0300

    add nullable fail reason to TimeSlotReservationResponse

--- MassTransit/MessageTypes/TimeSlotMessages.cs
+++ MassTransit/MessageTypes/TimeSlotMessages.cs
@@ -2,4 +2,4 @@ namespace Innowise.Clinic.Shared.MassTransit.MessageTypes;
 
 public record TimeSlotReservationRequest(Guid DoctorId, DateTime AppointmentStart, DateTime AppointmentEnd);
 
-public record TimeSlotReservationResponse(bool IsSuccessful, Guid? ReservedTimeSlotId);
+public record TimeSlotReservationResponse(bool IsSuccessful, Guid? ReservedTimeSlotId, string? FailReason);
