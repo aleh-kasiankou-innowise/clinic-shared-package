a81247f50b8360c67d64675ef4e2dc9691811e58
Aleh Kasiankou <oleg.kosenkov@innowise-group.com>
2023-03-06 14:17:11 +0300

    add UpdateAppointmentTimeslot request and response

--- MassTransit/MessageTypes/Requests/TimeSlotMessages.cs
+++ MassTransit/MessageTypes/Requests/TimeSlotMessages.cs
@@ -3,3 +3,7 @@ namespace Innowise.Clinic.Shared.MassTransit.MessageTypes.Requests;
 public record TimeSlotReservationRequest(Guid DoctorId, DateTime AppointmentStart, DateTime AppointmentEnd);
 
 public record TimeSlotReservationResponse(bool IsSuccessful, Guid? ReservedTimeSlotId, string? FailReason);
+
+public record UpdateAppointmentTimeslotRequest(Guid TimeSlotId, TimeSlotReservationRequest UpdatedTimeslotInfo);
+
+public record UpdateAppointmentTimeslotResponse(bool IsSuccessful, string? FailReason);
