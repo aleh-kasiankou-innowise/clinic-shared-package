96872a8618ea0ebebcc30df7840abba7ed3614b1
Aleh Kasiankou <oleg.kosenkov@innowise-group.com>
2023-03-17 10:08:12 +0300

    add improved EntityNotFoundException constructor

--- Exceptions/EntityNotFoundException.cs
+++ Exceptions/EntityNotFoundException.cs
@@ -4,6 +4,15 @@ public class EntityNotFoundException : ApplicationException
 {
     public EntityNotFoundException(string message) : base(message)
     {
-        
+    }
+
+    public EntityNotFoundException(string entityName, string searchParameter, string searchParameterValue) : base(
+        BuildExceptionMessage(entityName, searchParameter, searchParameterValue))
+    {
+    }
+
+    private static string BuildExceptionMessage(string entityName, string searchParameter, string searchParameterValue)
+    {
+        return $"The {entityName} with {searchParameter} : {searchParameterValue} is not registered in the system.";
     }
 }
