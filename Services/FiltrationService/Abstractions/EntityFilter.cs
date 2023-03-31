dbc326661998fe32a1b408e90dee9fd911e54ebd
“aleh-kasiankou-innowise” <oleg.kosenkov@innowise-group.com>
2023-03-31 11:45:59 +0300

    remove constructor with params for EntityFilter

--- Services/FiltrationService/Abstractions/EntityFilter.cs
+++ Services/FiltrationService/Abstractions/EntityFilter.cs
@@ -6,12 +6,6 @@ namespace Innowise.Clinic.Shared.Services.FiltrationService.Abstractions;
 [FilterKey("base")]
 public abstract class EntityFilter<T> : IEntityFilter<T>
 {
-    protected EntityFilter(string value)
-    {
-        Value = value;
-    }
-
-    public string Value { get; }
     public abstract Expression<Func<T, bool>> ToExpression(string value);
     
 }
