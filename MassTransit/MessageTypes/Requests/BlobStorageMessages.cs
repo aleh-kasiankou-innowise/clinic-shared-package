b5e5b79bb5f0cbd2a017e20ef1880c8fd6475338
“aleh-kasiankou-innowise” <oleg.kosenkov@innowise-group.com>
2023-05-04 14:14:31 +0300

    unified responses for BlobSaveRequest

--- MassTransit/MessageTypes/Requests/BlobStorageMessages.cs
+++ MassTransit/MessageTypes/Requests/BlobStorageMessages.cs
@@ -2,10 +2,8 @@ namespace Innowise.Clinic.Shared.MassTransit.MessageTypes.Requests;
 
 public record BlobSaveRequest(Guid FileId, string FileCategory, byte[] FileContent, string FileType);
 
-public record BlobUploadResponse(bool IsSuccessful, string? FileUrl, string? FailReason);
+public record BlobSaveResponse(bool IsSuccessful, string? FileUrl, string? FailReason);
 
 public record BlobDeletionRequest(string FileUrl);
 
 public record BlobDeletionResponse(bool IsSuccessful, string? FailReason);
-
-public record BlobUpdateResponse(bool IsSuccessful, string? FailReason);
