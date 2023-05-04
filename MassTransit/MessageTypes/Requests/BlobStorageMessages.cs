96e80015572e32e71112cf6289b0ad057d5cd0e0
“aleh-kasiankou-innowise” <oleg.kosenkov@innowise-group.com>
2023-05-04 13:12:21 +0300

    update BlobUploadRequest && BlobUpdateRequest

--- MassTransit/MessageTypes/Requests/BlobStorageMessages.cs
+++ MassTransit/MessageTypes/Requests/BlobStorageMessages.cs
@@ -1,6 +1,6 @@
 namespace Innowise.Clinic.Shared.MassTransit.MessageTypes.Requests;
 
-public record BlobUploadRequest(byte[] FileContent, string FileType, string FileCategory);
+public record BlobUploadRequest(Guid FileId, string FileCategory, byte[] FileContent, string FileType);
 
 public record BlobUploadResponse(bool IsSuccessful, string? FileUrl, string? FailReason);
 
@@ -8,6 +8,6 @@ public record BlobDeletionRequest(string FileUrl);
 
 public record BlobDeletionResponse(bool IsSuccessful, string? FailReason);
 
-public record BlobUpdateRequest(byte[] FileContent, string FileType, string FileUrl);
+public record BlobUpdateRequest(Guid FileId, string FileCategory, byte[] FileContent, string FileType);
 
 public record BlobUpdateResponse(bool IsSuccessful, string? FailReason);
