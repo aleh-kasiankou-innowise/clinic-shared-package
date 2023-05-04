76b05ed83bf4cfe4cab598bd901a64c669284070
“aleh-kasiankou-innowise” <oleg.kosenkov@innowise-group.com>
2023-05-04 14:10:55 +0300

    deleted AppointmentResultPdfGeneratedEvent + unified save / update messages for blob storage

--- MassTransit/MessageTypes/Events/AppointmentResultPdfGeneratedEvent.cs
+++
@@ -1,6 +0,0 @@
-using Innowise.Clinic.Shared.Enums;
-
-namespace Innowise.Clinic.Shared.MassTransit.MessageTypes.Events;
-
-public record AppointmentResultPdfGeneratedEvent(Guid AppointmentId, AppointmentResultChangeType ActionType,
-    byte[] File);
--- MassTransit/MessageTypes/Requests/BlobStorageMessages.cs
+++ MassTransit/MessageTypes/Requests/BlobStorageMessages.cs
@@ -1,13 +1,11 @@
 namespace Innowise.Clinic.Shared.MassTransit.MessageTypes.Requests;
 
-public record BlobUploadRequest(Guid FileId, string FileCategory, byte[] FileContent, string FileType);
+public record BlobSaveRequest(Guid FileId, string FileCategory, byte[] FileContent, string FileType);
 
 public record BlobUploadResponse(bool IsSuccessful, string? FileUrl, string? FailReason);
 
 public record BlobDeletionRequest(string FileUrl);
 
 public record BlobDeletionResponse(bool IsSuccessful, string? FailReason);
-
-public record BlobUpdateRequest(Guid FileId, string FileCategory, byte[] FileContent, string FileType);
 
 public record BlobUpdateResponse(bool IsSuccessful, string? FailReason);
