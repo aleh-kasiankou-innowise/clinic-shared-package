9a74878a81d9406a171e939f4b55f3dfd3b98368
“aleh-kasiankou-innowise” <oleg.kosenkov@innowise-group.com>
2023-05-04 13:52:52 +0300

    updated BlobCategories

--- Constants/BlobCategories.cs
+++ Constants/BlobCategories.cs
@@ -2,6 +2,7 @@ namespace Innowise.Clinic.Shared.Constants;
 
 public static class BlobCategories
 {
-    public static readonly string OfficePhoto = "office";
-    public static readonly string ProfilePhoto = "profile";
+    public const string OfficePhoto = "office";
+    public const string ProfilePhoto = "profile";
+    public const string AppointmentResultPdf = "appointment-result"; 
 }
